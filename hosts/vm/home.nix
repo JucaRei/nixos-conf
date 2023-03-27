d155a40ffcc38ec1d73374afeb5bb9c8613818c6
Reinaldo <reinaldo800@gmail.com>
2023-03-27 13:27:39 -0300

    testing

--- hosts/vm/home.nix
+++ hosts/vm/home.nix
@@ -20,6 +20,26 @@
       ../../modules/desktop/gnome/home.nix #Window Manager
     ];
 
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    # supportedLocales = [
+    #   "pt_BR.UTF-8"
+    #   "en_US.UTF-8"
+    # ];
+    extraLocaleSettings = {
+      # Extra locale settings that need to be overwritten
+      LANG = "en_US.UTF-8";
+      LC_TIME = "pt_BR.UTF-8";
+      LC_MONETARY = "pt_BR.UTF-8";
+      LC_NUMERIC = "pt_BR.UTF-8";
+      LC_NAME = "pt_BR.UTF-8";
+      LC_ADDRESS = "pt_BR.UTF-8";
+      LC_TELEPHONE = "pt_BR.UTF-8";
+      LC_MEASUREMENT = "pt_BR.UTF-8";
+      LC_ALL = "en_US.UTF-8";
+    };
+  };
+
   home = {
     # Specific packages for desktop
     packages = with pkgs; [
